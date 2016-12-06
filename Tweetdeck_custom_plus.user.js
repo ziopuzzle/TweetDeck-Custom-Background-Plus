b45b5dfefab7a5cab0fc06c9ca1be2e24b8ab2b0
ziopuzzle <ziopuzzle@gmail.com>
2016-12-06 22:43:57 +0900

    Update Tweetdeck_custom_plus.user.js

--- Tweetdeck_custom_plus.user.js
+++ Tweetdeck_custom_plus.user.js
@@ -11,9 +11,6 @@
 
 //このスクリプトはGoogleChromeでのみ動作します。
 
-/*
-https://somethingididnotknow.wordpress.com/2013/07/01/change-page-styles-with-greasemonkeytampermonkey/
-*/
 
 (function() {
     'use strict';

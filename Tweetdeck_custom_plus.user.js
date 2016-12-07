983fee34e478dc3a4481f9434c15004e3dfa8538
ziopuzzle <ziopuzzle@gmail.com>
2016-12-07 21:45:07 +0900

    Update Tweetdeck_custom_plus.user.js

--- Tweetdeck_custom_plus.user.js
+++ Tweetdeck_custom_plus.user.js
@@ -13,10 +13,10 @@
 
 
 (function() {
+    /* ------------------------------ 前準備 ------------------------------ */
     //Javascriptの厳格モードを使用
     'use strict';
-
-    //cssのグローバルスタイルでの記述を可能にする関数です。
+    //Javascriptでのcssグローバルスタイルの記述を可能にする関数です。
     function addGlobalStyle(css) {
         var head, style;
         head = document.getElementsByTagName('head')[0];
@@ -26,6 +26,8 @@
         style.innerHTML = css;
         head.appendChild(style);
     }
+    /* -------------------------- ここまで前準備 -------------------------- */
+
 
     var background_url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-460963.jpg"; //背景画像URL
 
@@ -66,15 +68,17 @@
 
     //ツイート更新時にエフェクトを付与
     var tweet_effect_flag = true;
-    var tweet_effect_time = 2.0; //エフェクトをかける時間
+    var tweet_effect_time = 2.0; //エフェクトをかける時間(秒)
+    var tweet_effect_color_st = 'rgba(128,128,200,0.7)'; //'rgba(50, 80, 108, 1.0)'; //開始エフェクト色
+    var tweet_effect_color_en = 'rgba(0,0,0,0.3)'; //終了エフェクト色
     if(tweet_effect_flag){
         addGlobalStyle(
             '@keyframes tweet-enter {' +
-            '      0% { background-color: rgba(50, 80, 108, 1.0); }' +
-            '     25% { background-color: rgba(50, 80, 108, 1.0); }' +
-            '     50% { background-color: rgba( 0,  0,   0, 0.3); }' +
-            '     75% { background-color: rgba(50, 80, 108, 1.0); }' +
-            '    100% { background-color: rgba( 0,  0,   0, 0.3); }' +
+            '      0% { background-color: '+tweet_effect_color_st+'; }' +
+            '     25% { background-color: '+tweet_effect_color_st+'; }' +
+            '     50% { background-color: '+tweet_effect_color_en+'; }' +
+            '     75% { background-color: '+tweet_effect_color_st+'; }' +
+            '    100% { background-color: '+tweet_effect_color_en+'; }' +
             '}'+
             '.js-chirp-container > article {' +
             '    animation-duration: '+tweet_effect_time+'s;' +
@@ -83,6 +87,26 @@
         );
     }
 
+    //テキスト色変更
+    var change_text_color_flag = true; //使用するかどうか(true/false)
+    var change_text_color_1 = 'rgba(255,200,80,1)'; // @user_id , #ハッシュタグ
+    if(change_text_color_flag){
+        addGlobalStyle(
+            //@user_id,#ハッシュタグ
+            '.column .link-complex,' +
+            '.column .hash,' +
+            '.column span.link-complex-target {' +
+            '    color: '+change_text_color_1+';' +
+            '    text-shadow: 2px 2px 1px #000000;' +
+            '}' +
+            //ユーザーネーム
+            '.column .account-link {' +
+            '    color:rgba(128,255,255,1);' +
+            '}'
+        );
+    }
+
+    //カラム透過
     addGlobalStyle(
         //カラム全体A
         '.will-animate {' +
@@ -99,8 +123,43 @@
         //引用ツイート
         '.js-quote-detail {' +
         '    background: rgba(0,0,0,0.5);' +
+        '}' +
+        //オプション
+        '.column-options,' +
+        '.js-search-filter {' +
+        '    background: rgba(0,0,0,0);' +
+        '}' +
+        '.js-accordion-item {' +
+        '    background: rgba(0,0,0,0.3);' +
+        '}' +
+        '.js-accordion-toggle-view {' +
+        '    background: rgba(0,0,0,0.7);' +
+        '}' +
+        '.accordion-header {' + //body
+        '    background: rgba(0,0,0,0.7);' +
+        '}' +
+        '.js-accordion-panel {' + //折りたたみメニュー - body
+        '    background: rgba(0,0,0,0.7);' +
+        '}' +
+        '.btn-options-tray-space {' + //折りたたみメニュー - Edit collection
+        '    background: rgba(0,0,0,0.7);' +
+        '}' +
+        '.is-active {' + //折りたたみメニュー - OPEN時 (Stylebot専用)
+        '    background: rgba(0,0,0,0.3);' +
+        '}' +
+        '.accordion-divider-t {' + //折りたたみメニュー - フッター (Stylebot専用)
+        '    background: rgba(0,0,0,0.7);' +
+        '}' +
+        '.is-options-open .column-settings-link {' +   //折りたたみメニュー - オプションボタン
+        '    background-color: rgba(  0,  0,  0,0);' + //ボタン背景
+        '    color:            rgba(136,187,221,1);' + //ボタン押された時のアイコンの色
+        '    border-color:     rgba( 34, 36, 38,1);' + //ボタン押された時の枠の色
+        '    border-bottom: none;' +
         '}'
     );
+
+
+
 
 })();
 
